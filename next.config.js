00204678951d5827b79514e66bcc89e850e682e7
Carlos Engel <carlosengel011@gmail.com>
2020-10-02 16:06:19 -0400

    try to push without erros :)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles') , path.join(__dirname , 'components')],
+    includePaths: [path.join(__dirname, 'styles') , path.join(__dirname , 'components/')],
   },
+  pageExtensions: ['jsx', 'js', 'ts', 'tsx' , 'scss'],
 }
